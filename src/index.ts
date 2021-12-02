f2b593e5c36c8d18220509fdc5f52843e4561533
Sun <wei01.sun@sap.com>
2021-12-02 09:29:27 +0800

    add prettier support

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import * as StringUtilities from "./utilities/stringUtilities";
 import * as NumberUtilities from "./utilities/numberUtilities";
 // hi;  //if this line is enabled, the command type-check should report error
-let sGreeting = "hello";    //if this line reports error, that means eslint works
-let sConvertedGreeding = StringUtilities.toUpperCase(sGreeting);  //if this line reports error, that means eslint works
+let sGreeting = "hello"; //if this line reports error, that means eslint works
+let sConvertedGreeding = StringUtilities.toUpperCase(sGreeting); //if this line reports error, that means eslint works
 console.log(sConvertedGreeding + " world"); //if this line reports warnning, that means eslint works
-
