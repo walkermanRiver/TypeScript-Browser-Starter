df421bb5a7e3c404683bc13fa1f4264cf1d63286
Sun <wei01.sun@sap.com>
2021-12-03 08:14:29 +0800

    support tsx

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,7 +1,7 @@
-import { add } from '../src/public/utilities/numberUtilities';
+import { add } from "../src/public/utilities/numberUtilities";
 
-if( add(1,2) === 3) {
-    console.log("pass")
+if (add(1, 2) === 3) {
+  console.log("pass");
 } else {
-    console.log("failed");
+  console.log("failed");
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,39 @@
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-    entry: './src/index.ts',
-    mode: 'development',
-    plugins: [
-        new CleanWebpackPlugin({
-          cleanAfterEveryBuildPatterns: ['dist/build']
-        }),
-        new HtmlWebpackPlugin({
-          template: 'src/templates/index.html'
-        }),
+  entry: ["./src/index.ts", "./src/app/app.tsx"],
+  mode: "development",
+  devtool: "source-map",
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ["dist/build"],
+    }),
+    new HtmlWebpackPlugin({
+      template: "src/templates/index.html",
+    }),
+  ],
+  output: {
+    // path:path.resolve(__dirname, 'dist'),
+    path: __dirname + "/dist",
+    filename: "build/[name].[contenthash].js",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "babel-loader",
+        exclude: "/node_modules",
+      },
     ],
-    output:{
-        // path:path.resolve(__dirname, 'dist'),
-        path: __dirname + '/dist',
-        filename: 'build/[name].[contenthash].js'        
-    },
-    resolve: {
-        extensions:['.tsx','.ts','.jsx','.js']
-    },
-    module:{
-        rules:[
-            {
-                test: /\.tsx?$/,
-                use: 'babel-loader',
-                exclude: '/node_modules'
-            }
-        ]
-    },
-    devServer: {
-        static: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 9000
-    } 
+  },
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
 };
